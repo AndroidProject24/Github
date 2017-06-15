5fb918049da0750007d815e735b8da7ec637618e
ToanIT <huynhvantoan.itc@gmail.com>
2017-06-15 17:00:52 +0700

    cat dat kotlin.

---
+++ core/src/main/kotin/com/toanit/core/utils/SnackBarUtils.kt
@@ -0,0 +1,30 @@
+package com.aidchow.doubanmovie.util
+
+import android.app.Activity
+import android.content.Context
+import android.support.annotation.StringRes
+import android.support.design.widget.Snackbar
+import android.view.View
+import android.widget.Toast
+
+/**
+ * Created by aidchow on 17-5-29.
+ */
+object SnackBarUtils {
+    fun showSnackBar(v: View?, snackbarText: String?) {
+        if (v == null || snackbarText == null) {
+            return
+        }
+        Snackbar.make(v, snackbarText, Snackbar.LENGTH_LONG).show()
+    }
+
+    fun Context.showToast(@StringRes text: Int) = Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+
+    fun Activity.showToast(msg:String) {
+        Toast.makeText(applicationContext,msg, Toast.LENGTH_SHORT).show()
+    }
+
+    fun Activity.showToast(@StringRes msgId:Int) {
+        Toast.makeText(applicationContext,msgId, Toast.LENGTH_SHORT).show()
+    }
+}

c2667cde1f7f29bd0128b697fadadf21334327c0
ToanIT <huynhvantoan.itc@gmail.com>
2017-06-28 18:48:55 +0700

    Remove file.

--- core/src/main/kotin/com/toanit/core/utils/RRx.kt
+++
@@ -1,9 +0,0 @@
-package net.danlew.counter.util
-
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
-
-operator fun CompositeDisposable.plus(disposable: Disposable): CompositeDisposable {
-  add(WeakReference(disposable))
-  return this
-}
